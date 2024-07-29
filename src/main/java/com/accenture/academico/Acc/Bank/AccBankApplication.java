0666b41bbe8d74e58abe89fe3a98fa54c34895d9
thiago azvdo <thiagoazvdosoares@hotmail.com>
2024-07-29 18:12:13 -0300

    subindo arquivos

---
+++ src/main/java/com/accenture/academico/Acc/Bank/AccBankApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.academico.Acc.Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accenture/academico/Acc/Bank/AccBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.academico.Acc.Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
