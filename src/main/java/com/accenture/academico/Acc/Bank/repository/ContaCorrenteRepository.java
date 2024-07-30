5cb820872301689dda470fd6f1ce3af14bf395bd
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2024-07-30 20:48:19 -0300

    feat: adiciona metodo de bucar ContaCorrente por numero

--- src/main/java/com/accenture/academico/Acc/Bank/repository/ContaCorrenteRepository.java
+++ src/main/java/com/accenture/academico/Acc/Bank/repository/ContaCorrenteRepository.java
@@ -1,5 +1,7 @@
 package com.accenture.academico.Acc.Bank.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.accenture.academico.Acc.Bank.model.ContaCorrente;
 @Repository
 public interface ContaCorrenteRepository extends JpaRepository<ContaCorrente, Long>{
 
+	Optional<ContaCorrente> findByNumero(String numero);
 }
