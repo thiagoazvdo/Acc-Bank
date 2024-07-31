e22c52b18f9ef46e16f1034905cdb42ce791f3b4
thiago azvdo <thiagoazvdosoares@hotmail.com>
2024-07-31 11:58:22 -0300

    feat: adiciona AgenciaRepository

---
+++ src/main/java/com/accenture/academico/Acc/Bank/repository/AgenciaRepository.java
@@ -0,0 +1,9 @@
+package com.accenture.academico.Acc.Bank.repository;
+
+import com.accenture.academico.Acc.Bank.model.Agencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgenciaRepository extends JpaRepository<Agencia, Long> {
+}
