66ab9b66c32c4ceedbe620897ee5b9ef060e2708
thiago azvdo <thiagoazvdosoares@hotmail.com>
2024-07-30 21:56:05 -0300

    refactor: ClienteController refatorado

--- src/main/java/com/accenture/academico/Acc/Bank/controller/ClienteController.java
+++ src/main/java/com/accenture/academico/Acc/Bank/controller/ClienteController.java
@@ -1,7 +1,7 @@
 package com.accenture.academico.Acc.Bank.controller;
-import com.accenture.academico.Acc.Bank.expection.ClienteNaoEncontradoException;
 import com.accenture.academico.Acc.Bank.model.Cliente;
 import com.accenture.academico.Acc.Bank.service.ClienteService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
@@ -23,36 +21,31 @@ public class ClienteController {
 
 
     @GetMapping
-    public List<Cliente> listar() {
-        return clienteService.listar();
+    public ResponseEntity<?> listar() {
+        return ResponseEntity.status(HttpStatus.OK).body(clienteService.listar());
     }
 
     @GetMapping("/{id}")
-    public Cliente buscar(@PathVariable Long id) throws ClienteNaoEncontradoException {
-        return clienteService.buscarOuFalhar(id);
+    public ResponseEntity<?> buscar(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(clienteService.buscarCliente(id));
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente adicionar(@RequestBody Cliente cliente){
-        System.out.println("CLIENTE RECEBIDO: " + cliente);
-        return clienteService.salvar(cliente);
+    public ResponseEntity<?> adicionar(@RequestBody Cliente cliente){
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.salvar(cliente));
+
     }
 
     @PutMapping("/{clienteId}")
-    public Cliente atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) throws ClienteNaoEncontradoException{
-        Cliente clienteAtual = clienteService.buscarOuFalhar(clienteId);
-        clienteAtual.setNome(cliente.getNome());
-        if (cliente.getTelefone() != null) {
-            clienteAtual.setTelefone(cliente.getTelefone());
-        }
-        return clienteService.salvar(clienteAtual);
+    public ResponseEntity<?> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+        return ResponseEntity.status(HttpStatus.OK).body(clienteService.atualizar(clienteId, cliente));
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+
     @DeleteMapping("/{clienteId}")
-    public void remover(@PathVariable Long clienteId){
+    public ResponseEntity<?> remover(@PathVariable Long clienteId){
         clienteService.excluir(clienteId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
