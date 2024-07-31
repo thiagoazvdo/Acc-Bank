e0152e08d59e876b97f69d79ac914af286605bbe
thiago azvdo <thiagoazvdosoares@hotmail.com>
2024-07-30 22:42:45 -0300

    fix: correção de bug de exceptions

--- src/main/java/com/accenture/academico/Acc/Bank/expection/ClienteNaoEncontradoException.java
+++ src/main/java/com/accenture/academico/Acc/Bank/expection/ClienteNaoEncontradoException.java
@@ -3,8 +3,7 @@ package com.accenture.academico.Acc.Bank.expection;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ClienteNaoEncontradoException extends NegocioException {
+public class ClienteNaoEncontradoException extends BancoException {
 
     public ClienteNaoEncontradoException() {
         super("Não existe um cliente cadastrado com o CPF informado");
--- src/main/java/com/accenture/academico/Acc/Bank/expection/NegocioException.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.academico.Acc.Bank.expection;
-
-public class NegocioException extends RuntimeException {
-
-    public NegocioException(String mensagem) {
-        super(mensagem);
-    }
-
-}
