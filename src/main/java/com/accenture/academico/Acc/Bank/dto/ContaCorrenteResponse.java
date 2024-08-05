13a302efcaeb90b04099cc6c1056ca3528afe4a4
thiago azvdo <thiagoazvdosoares@hotmail.com>
2024-08-05 18:41:42 -0300

    refactor: removendo import e método desnecessário

--- src/main/java/com/accenture/academico/Acc/Bank/dto/ContaCorrenteResponse.java
+++ src/main/java/com/accenture/academico/Acc/Bank/dto/ContaCorrenteResponse.java
@@ -1,7 +1,6 @@
 package com.accenture.academico.Acc.Bank.dto;
 
 import com.accenture.academico.Acc.Bank.model.Agencia;
-import com.accenture.academico.Acc.Bank.model.ContaCorrente;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,11 +20,4 @@ public class ContaCorrenteResponse {
 
     private Agencia agencia;
 
-    public ContaCorrenteResponse toEntity(ContaCorrente contaCorrente) {
-        this.id = contaCorrente.getId();
-        this.numero = contaCorrente.getNumero();
-        this.saldo = contaCorrente.getSaldo();
-        this.agencia = contaCorrente.getAgencia();
-        return this;
-    }
 }
