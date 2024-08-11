a28a5b0f2df32e782ed3517092e9adc6ab0ca5d3
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2024-08-11 00:44:18 -0300

    feature: adiciona feature de obterExtratoGeral

--- src/main/java/com/accenture/academico/Acc/Bank/service/TransacaoService.java
+++ src/main/java/com/accenture/academico/Acc/Bank/service/TransacaoService.java
@@ -5,6 +5,7 @@ import java.time.YearMonth;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import com.accenture.academico.Acc.Bank.model.ContaCorrente;
@@ -14,11 +15,22 @@ import com.accenture.academico.Acc.Bank.repository.TransacaoRepository;
 @Service
 public class TransacaoService {
 
+	@Lazy
 	@Autowired
 	private ContaCorrenteService contaCorrenteService;
 	
 	@Autowired
 	private TransacaoRepository transacaoRepository;
+
+	public List<Transacao> obterExtratoGeral(Long contaId){
+    	ContaCorrente conta = contaCorrenteService.buscarContaCorrente(contaId);
+    	return transacaoRepository.findByContaCorrente(conta);
+    }
+	
+	public List<Transacao> obterExtratoFiltrado(Long contaId, LocalDateTime dataInicio, LocalDateTime dataFim){
+		ContaCorrente conta = contaCorrenteService.buscarContaCorrente(contaId);
+		return transacaoRepository.findByContaCorrenteAndDataHoraBetween(conta, dataInicio, dataFim);
+	}
     
     public List<Transacao> obterExtratoMensal(Long contaId, YearMonth mesAno){
     	LocalDateTime mesInicio = mesAno.atDay(1).atStartOfDay();
@@ -34,8 +46,4 @@ public class TransacaoService {
     	return obterExtratoFiltrado(contaId, anoInicio, anoFim);
     }
     
-    public List<Transacao> obterExtratoFiltrado(Long contaId, LocalDateTime dataInicio, LocalDateTime dataFim){
-    	ContaCorrente conta = contaCorrenteService.buscarContaCorrente(contaId);
-    	return transacaoRepository.findByContaCorrenteAndDataHoraBetween(conta, dataInicio, dataFim);
-    }
 }
